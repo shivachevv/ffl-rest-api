48414d5065265daf28c99e3a8378561a6209268c
shivachevv <31926786+shivachevv@users.noreply.github.com>
2020-12-01 14:38:43 +0200

    heroku test

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const env = process.env.ENV || 'development'
 
 const config = {
     development: {
-        port: process.env.PORT || 4000,
+        port: process.env.PORT || 5000,
         secret: "I_SHOT_JFK",
         databaseUrl: 'https://ffl-3-new.firebaseio.com/',
         apiRootRoute: '/api/'
